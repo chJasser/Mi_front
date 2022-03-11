338e99ef8c1785a086d54dce19c519f0b70fc12d
fakher14 <73257003+fakher14@users.noreply.github.com>
2022-03-11 15:01:33 +0100

    cartslice

---
+++ src/app/productslice/Productslice.js
@@ -0,0 +1,77 @@
+import { createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
+let initialState = {
+products: [],
+selectedProduct: {},
+errors: "",
+};
+
+const ProductsSlice = createSlice({
+name: "products",
+initialState,
+reducers: {
+populateProducts(state, action) {
+state.products = action.payload;
+},
+selectProduct(state, action) {
+state.selectedProduct = action.payload;
+},
+unselectProduct(state) {
+state.selectedProduct = null;
+},
+deleteProduct: (state, action) => {
+    //payload :id
+const payload = action.payload;
+const index = state.products.findIndex((item) => item._id === payload);
+if (index !== -1) {
+state.products.splice(index, 1);
+}
+},
+updateProduct: (state, action) => {
+const payload = action.payload;
+const index = state.products.findIndex(
+(item) => item._id === payload._id
+);
+if (index !== -1) {
+state.products[index] = payload;
+}
+},
+addProduct: (state, action) => {
+const payload = action.payload;
+state.products.push(payload);
+},
+setErrors(state, action) {
+state.errors = action.payload;
+},
+},
+});
+export const fetchProducts = () =>(dispatch) => {
+//const [res, error] = await queryApi("products");
+ axios.get("/products/filter").then((res)=>{
+    dispatch(populateProducts(res));
+}).catch((err)=>{
+    dispatch(setErrors(err))
+});
+/*if (error) {
+dispatch(setErrors(error));
+} else {
+
+dispatch(populateProducts(data));
+}*/
+};
+export const selectProducts = (state) => {
+return [state.products.products, state.products.errors];
+};
+export const selectSelectedProduct = (state) => {
+return state.products.selectedProduct;
+};
+export const {
+populateProducts,
+selectProduct,
+unselectProduct,
+setErrors,
+deleteProduct,
+updateProduct,
+addProduct,
+} = productsSlice.actions;
+export default productsSlice.reducer;
