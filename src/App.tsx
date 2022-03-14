f678848f029690c95be38290d2f79f4da79ea7e9
chJasser <chaieb.jasser@esprit.tn>
2022-03-14 13:13:48 +0100

    login logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
+import { logoutUser, setCurrentUser } from "app/slices/userSlice";
+import { store } from "app/store";
 import { setAuthToken } from "axiosInstance";
 import MyRouter from "routers/index";
+import jwt_decode from "jwt-decode";
 if (localStorage.token) {
   // Set auth token header auth
   const token = localStorage.token;
   setAuthToken(token);
   // Decode token and get user info and exp
-  // const decoded = jwt_decode(token);
-  // // Set user and isAuthenticated
-  // store.dispatch(setCurrentUser(decoded));
+
+  const decoded: any = jwt_decode(token);
+
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
   // // Check for expired token
 
-  // const currentTime = Date.now() / 1000; // to get in milliseconds
-  // if (decoded.exp < currentTime) {
-  //   // Logout user
-  //   store.dispatch(logoutUser());
-  //   // Redirect to login
-  //   window.location.href = "./sign-in";
-  // }
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.expiresIn < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
 }
 
 export default function App() {
