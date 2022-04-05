4fce8c81f018a82ee15eb486aac8fd75afe948ac
chJasser <chaieb.jasser@esprit.tn>
2022-04-06 00:29:26 +0100

    rating

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -90,7 +90,7 @@ export default function Sidebar() {
                   className={
                     "text-xs uppercase py-3 font-bold block " +
                     (window.location.href.indexOf("/back-office/dashboard") !==
-                    -1
+                      -1
                       ? "text-lightBlue-500 hover:text-lightBlue-600"
                       : "text-blueGray-700 hover:text-blueGray-500")
                   }
@@ -115,7 +115,7 @@ export default function Sidebar() {
                   className={
                     "text-xs uppercase py-3 font-bold block " +
                     (window.location.href.indexOf("/back-office/settings") !==
-                    -1
+                      -1
                       ? "text-lightBlue-500 hover:text-lightBlue-600"
                       : "text-blueGray-700 hover:text-blueGray-500")
                   }
@@ -125,7 +125,7 @@ export default function Sidebar() {
                     className={
                       "fas fa-tools mr-2 text-sm " +
                       (window.location.href.indexOf("/back-office/settings") !==
-                      -1
+                        -1
                         ? "opacity-75"
                         : "text-blueGray-300")
                     }
@@ -148,7 +148,7 @@ export default function Sidebar() {
                     className={
                       "fas fa-table mr-2 text-sm " +
                       (window.location.href.indexOf("/back-office/tables") !==
-                      -1
+                        -1
                         ? "opacity-75"
                         : "text-blueGray-300")
                     }
@@ -156,6 +156,30 @@ export default function Sidebar() {
                   Tables
                 </Link>
               </li>
+
+              <li className="items-center">
+                <Link
+                  className={
+                    "text-xs uppercase py-3 font-bold block " +
+                    (window.location.href.indexOf("/back-office/productstable") !== -1
+                      ? "text-lightBlue-500 hover:text-lightBlue-600"
+                      : "text-blueGray-700 hover:text-blueGray-500")
+                  }
+                  to="/back-office/productstable"
+                >
+                  <i
+                    className={
+                      "fas fa-table mr-2 text-sm " +
+                      (window.location.href.indexOf("/back-office/productstable") !==
+                        -1
+                        ? "opacity-75"
+                        : "text-blueGray-300")
+                    }
+                  ></i>{" "}
+                  Manage Product
+                </Link>
+              </li>
+
             </ul>
 
             {/* Divider */}
